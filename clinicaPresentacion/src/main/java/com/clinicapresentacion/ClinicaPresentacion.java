b6b0f2b922da9d3b46bd79ed9d775f20dde79c5b
DeniseGarc <aliciadenise0623@gmail.com>
2025-02-18 12:34:10 -0700

    commit inicial

---
+++ clinicaPresentacion/src/main/java/com/clinicapresentacion/ClinicaPresentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.clinicapresentacion;
+
+/**
+ *
+ * @author Alici
+ */
+public class ClinicaPresentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
