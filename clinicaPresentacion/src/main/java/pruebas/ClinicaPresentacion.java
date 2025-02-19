30139038f44754ed47dc08dd433738387b0489fb
DeniseGarc <aliciadenise0623@gmail.com>
2025-02-19 00:58:27 -0700

    Carcasa de las pantallas
    Se realizó la estructura de las pantallas del programa, por lo que aun falta implementar la funcionalidad.

--- clinicaPresentacion/src/main/java/pruebas/ClinicaPresentacion.java
+++ clinicaPresentacion/src/main/java/pruebas/ClinicaPresentacion.java
@@ -4,6 +4,8 @@
 
 package pruebas;
 
+import GUI.FrmRegistro;
+
 /**
  *
  * @author Alici
@@ -11,6 +13,6 @@ package pruebas;
 public class ClinicaPresentacion {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        FrmRegistro frm = new  FrmRegistro();
     }
 }
