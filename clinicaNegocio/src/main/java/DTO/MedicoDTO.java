ebd1d84dcc8b829b09dad6b4bb91d3ef99e02cc2
DeniseGarc <aliciadenise0623@gmail.com>
2025-02-20 21:32:21 -0700

    Se eliminó horarioDTO
    Se eliminó horarioDTO y se comienza a cambiar MedicoDTO.

--- clinicaNegocio/src/main/java/DTO/MedicoDTO.java
+++ clinicaNegocio/src/main/java/DTO/MedicoDTO.java
@@ -1,6 +1,5 @@
 package DTO;
 
-import java.util.List;
 
 /**
  *
@@ -13,96 +12,8 @@ public class MedicoDTO {
     private String apellidoMaternoMedico;
     private String cedulaProfesional;
     private String especialidad;
-    private String esstado;
-    private List<HorarioDTO> horarios;
+    private String estado;
 
-    public MedicoDTO() {
-    }
-
-    public MedicoDTO(String IDMedico, String nombresMedico, String apellidoPaternoMedico, String apellidoMaternoMedico, String cedulaProfesional, String especialidad, String esstado, List<HorarioDTO> horarios) {
-        this.IDMedico = IDMedico;
-        this.nombresMedico = nombresMedico;
-        this.apellidoPaternoMedico = apellidoPaternoMedico;
-        this.apellidoMaternoMedico = apellidoMaternoMedico;
-        this.cedulaProfesional = cedulaProfesional;
-        this.especialidad = especialidad;
-        this.esstado = esstado;
-        this.horarios = horarios;
-    }
-
-    public MedicoDTO(String nombresMedico, String apellidoPaternoMedico, String apellidoMaternoMedico, String cedulaProfesional, String especialidad, String esstado, List<HorarioDTO> horarios) {
-        this.nombresMedico = nombresMedico;
-        this.apellidoPaternoMedico = apellidoPaternoMedico;
-        this.apellidoMaternoMedico = apellidoMaternoMedico;
-        this.cedulaProfesional = cedulaProfesional;
-        this.especialidad = especialidad;
-        this.esstado = esstado;
-        this.horarios = horarios;
-    }
-
-    public String getIDMedico() {
-        return IDMedico;
-    }
-
-    public void setIDMedico(String IDMedico) {
-        this.IDMedico = IDMedico;
-    }
-
-    public String getNombresMedico() {
-        return nombresMedico;
-    }
-
-    public void setNombresMedico(String nombresMedico) {
-        this.nombresMedico = nombresMedico;
-    }
-
-    public String getApellidoPaternoMedico() {
-        return apellidoPaternoMedico;
-    }
-
-    public void setApellidoPaternoMedico(String apellidoPaternoMedico) {
-        this.apellidoPaternoMedico = apellidoPaternoMedico;
-    }
-
-    public String getApellidoMaternoMedico() {
-        return apellidoMaternoMedico;
-    }
-
-    public void setApellidoMaternoMedico(String apellidoMaternoMedico) {
-        this.apellidoMaternoMedico = apellidoMaternoMedico;
-    }
-
-    public String getCedulaProfesional() {
-        return cedulaProfesional;
-    }
-
-    public void setCedulaProfesional(String cedulaProfesional) {
-        this.cedulaProfesional = cedulaProfesional;
-    }
-
-    public String getEspecialidad() {
-        return especialidad;
-    }
-
-    public void setEspecialidad(String especialidad) {
-        this.especialidad = especialidad;
-    }
-
-    public String getEsstado() {
-        return esstado;
-    }
-
-    public void setEsstado(String esstado) {
-        this.esstado = esstado;
-    }
-
-    public List<HorarioDTO> getHorarios() {
-        return horarios;
-    }
-
-    public void setHorarios(List<HorarioDTO> horarios) {
-        this.horarios = horarios;
-    }
     
     
 }
